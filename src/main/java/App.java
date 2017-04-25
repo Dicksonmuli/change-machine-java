691d4fa36bf6b7444c5e5754e0e419d175ac3d62
dicksonmuli <dicksonmully6@gmail.com>
2017-04-25 06:34:26 +0300

    Adding resources

---
+++ src/main/java/App.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args){
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/results.vtl");
+      Float moneyInput = Float.parseFloat(request.queryParams("monies"));
+      ChangeMachine needChange = new ChangeMachine();
+      String changeMade = needChange.makeChange(moneyInput);
+      model.put("resultSpot", changeMade);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+}
